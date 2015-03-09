6b69878870f7f4f71613357011f681888aad89b7
unknown <atrainor@HP-ATRAINOR1.captechventures.com>
2015-03-08 20:23:04 -0400

    read csv

--- PredictKeypointPositionsDriver.py
+++ PredictKeypointPositionsDriver.py
@@ -6,7 +6,39 @@ Created on Sun Mar  1 19:48:41 2015
 """
 
 import csv
+import numpy as np
 
 
+training = {}
+test = {}
+lookup = {}
+
+
+with open('data/training/training.csv', 'rb') as train_csv:
+    reader = csv.reader(train_csv)
+    key = 0    
+    for row in reader:
+        training[key] = row
+        key = key + 1
+        
+with open('data/test/test.csv', 'rb') as test_csv:
+    reader = csv.reader(test_csv)
+    key = 0    
+    for row in reader:
+        test[key] = row
+        key = key + 1
+        
+        
+with open('data/IDLookupTable.csv', 'rb') as ids_csv:
+    reader = csv.reader(ids_csv)
+    key = 0    
+    for row in reader:
+        lookup[key] = row
+        key = key + 1
+        
+
+        
+
+        
 
 
