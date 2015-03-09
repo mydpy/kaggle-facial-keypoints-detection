d9bb5cfe70fb411da69a6e63dfe1907c9e5011ac
unknown <atrainor@HP-ATRAINOR1.captechventures.com>
2015-03-08 20:58:49 -0400

    process image

--- PredictKeypointPositionsDriver.py
+++ PredictKeypointPositionsDriver.py
@@ -5,6 +5,28 @@ Created on Sun Mar  1 19:48:41 2015
 @author: mbaker
 """
 
+"""
+import numpy as np
+from scipy.misc import *
+from PIL import *
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.cm as cm
+a = training[1]
+image = a[30]
+image = image.split(" ")
+image2 = np.reshape(image, (96,96))
+image2 = image2.astype(np.float)
+imgplot = plt.imshow(image2, cmap = cm.Greys_r)
+"""
+
+
+
+
+
+
+
+
 import csv
 import numpy as np
 
